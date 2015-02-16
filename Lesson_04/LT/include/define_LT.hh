c2efd5fec48f746c7a7f68670b0ca229d2873331
Nicolò Danzi <nicolodanzi@msn.com>
2015-02-16 12:37:47 +0100

    aggiunto TLM LT

---
+++ Lesson_04/LT/include/define_LT.hh
@@ -0,0 +1,14 @@
+#ifndef __define_LT_h__
+#define __define_LT_h__
+
+#include <systemc.h>
+
+struct iostruct {
+  sc_lv<64> number1;
+  sc_lv<64> number2;
+  sc_lv<64> result;
+};
+#define ADDRESS_TYPE int
+#define DATA_TYPE iostruct
+
+#endif
